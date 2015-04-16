068e560828230c64378d4607e97542fb8ecc0eab
Mike Klemarewski <mikek@mobify.com>
2015-04-16 11:41:21 -0700

    Fix script

--- site/public/js/cart.js
+++ site/public/js/cart.js
@@ -1,11 +1,9 @@
-<script>
-    $(".add-to-cart").click(function() {
-        var item = $(this).closest('.item')[0];
-        var image = $(item).find('img')[0];
+$(".add-to-cart").click(function() {
+    var item = $(this).closest('.item')[0];
+    var image = $(item).find('img')[0];
 
-        var cart = JSON.parse(localStorage.getItem("cart")) || [];
-        cart.push({item: item.dataset.item, image: image.src});
+    var cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push({item: item.dataset.item, image: image.src});
 
-        localStorage.setItem("cart", JSON.stringify(cart));
-    });
-</script>
+    localStorage.setItem("cart", JSON.stringify(cart));
+});
