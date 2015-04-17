1fdf551956d1fd02e3aac641ef5fc02850cde641
Shawn Jansepar <shawn@mobify.com>
2015-04-16 17:43:57 -0700

    Stop loading environment variables by sourcing *rc files

--- app/scaffold/build-js.sh
+++ app/scaffold/build-js.sh
@@ -6,11 +6,9 @@
 # environment variables. This blows up when we expect some command like 'grunt'
 # to be available in your PATH. Source all files that typically update the PATH
 # to ensure that Gradle has the same PATH as your usual terminal.
-source ~/.zshrc
-source ~/.bashrc
-source ~/.bash_profile
-
 set -e
+
+echo "PATH: " + $PATH
 
 # Build app.js.
 pushd ../app
