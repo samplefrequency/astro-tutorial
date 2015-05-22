45da95a6138267cce550c23669bd23fa29e868d0
Mike Klemarewski <mikek@mobify.com>
2015-05-22 15:45:42 -0700

    Add worker to config.js

--- native-app/app/config.js
+++ native-app/app/config.js
@@ -2,10 +2,11 @@ require.config({
     baseUrl: '.',
     paths: {
     	'astro': '../node_modules/astro-sdk/js/src/astro',
-      'app': '../node_modules/astro-sdk/js/src/app',
+        'app': '../node_modules/astro-sdk/js/src/app',
+        'bluebird': 'node_modules/bluebird/js/browser/bluebird',
     	'plugin-manager': '../node_modules/astro-sdk/js/src/plugin-manager',
     	'plugins': '../node_modules/astro-sdk/js/src/plugins',
-    	'vendor/backbone-events': '../node_modules/astro-sdk/js/vendor/backbone-events',
-      'bluebird': 'node_modules/bluebird/js/browser/bluebird'
+        'vendor/backbone-events': '../node_modules/astro-sdk/js/vendor/backbone-events',
+        'worker': '../node_modules/astro-sdk/js/worker'
     }
 });
