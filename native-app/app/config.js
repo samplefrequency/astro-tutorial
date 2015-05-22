59ec0b5a187c959151564e50faaa64198430d46d
Justin Vaillancourt <justinvaillancourt@gmail.com>
2015-05-22 15:55:39 -0700

    Fix b0rked config.js

--- native-app/app/config.js
+++ native-app/app/config.js
@@ -1,12 +1,11 @@
 require.config({
     baseUrl: '.',
     paths: {
-    	'astro': '../node_modules/astro-sdk/js/src/astro',
-        'app': '../node_modules/astro-sdk/js/src/app',
+        'astro': '../node_modules/astro-sdk/js/src/astro',
         'bluebird': 'node_modules/bluebird/js/browser/bluebird',
-    	'plugin-manager': '../node_modules/astro-sdk/js/src/plugin-manager',
-    	'plugins': '../node_modules/astro-sdk/js/src/plugins',
+        'plugin-manager': '../node_modules/astro-sdk/js/src/plugin-manager',
+        'plugins': '../node_modules/astro-sdk/js/src/plugins',
         'vendor/backbone-events': '../node_modules/astro-sdk/js/vendor/backbone-events',
-        'worker': '../node_modules/astro-sdk/js/worker'
+        'worker': '../node_modules/astro-sdk/js/src/worker'
     }
 });
