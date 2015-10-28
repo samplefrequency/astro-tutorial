f3d9116a888edfa215d64cd7008ff54e2bc4e76b
Liz Cross <elizabeth@mobify.com>
2015-10-28 16:30:56 -0700

    Adding debugging functionality to app.js.

--- app/app.js
+++ app/app.js
@@ -1,40 +1,46 @@
-require([
-  'astro-full',
-  'bluebird',
-  'application',
-  'plugins/webViewPlugin',
-  'plugins/anchoredLayoutPlugin',
-  'plugins/navigationPlugin'
-],
-function(
-  Astro,
-  Promise,
-  Application,
-  WebViewPlugin,
-  AnchoredLayoutPlugin,
-  NavigationPlugin
-) {
+window.AstroMessages = []; // For debugging messages
 
-  // Enter your site url here
-  var baseUrl = 'http://<Your IP Address>:5000';
+window.run = function() {
+    require([
+      'astro-full',
+      'bluebird',
+      'application',
+      'plugins/webViewPlugin',
+      'plugins/anchoredLayoutPlugin',
+      'plugins/navigationPlugin'
+    ],
+    function(
+      Astro,
+      Promise,
+      Application,
+      WebViewPlugin,
+      AnchoredLayoutPlugin,
+      NavigationPlugin
+    ) {
 
-  // Initialize plugins
-  var mainNavigationPromise = NavigationPlugin.init();
-  var layoutPromise         = AnchoredLayoutPlugin.init();
+      // Enter your site url here
+      var baseUrl = 'http://<Your IP Address>:5000';
 
-  // Start the app by navigating to the base url
-  mainNavigationPromise.then(function(mainNavigationView){
-    mainNavigationView.navigate(baseUrl);
-  });
+      // Initialize plugins
+      var mainNavigationPromise = NavigationPlugin.init();
+      var layoutPromise         = AnchoredLayoutPlugin.init();
 
-  // Use the mainNavigationView as the main content view for our layout
-  Promise.join(layoutPromise, mainNavigationPromise, function(layout, mainNavigationView) {
-    layout.setContentView(mainNavigationView);
-  });
+      // Start the app by navigating to the base url
+      mainNavigationPromise.then(function(mainNavigationView){
+        mainNavigationView.navigate(baseUrl);
+      });
 
-  // Set our layout as the main view of the app
-  layoutPromise.then(function(layout){
-    Application.setMainViewPlugin(layout);
-  });
+      // Use the mainNavigationView as the main content view for our layout
+      Promise.join(layoutPromise, mainNavigationPromise, function(layout, mainNavigationView) {
+        layout.setContentView(mainNavigationView);
+      });
 
-}, undefined, true);
+      // Set our layout as the main view of the app
+      layoutPromise.then(function(layout){
+        Application.setMainViewPlugin(layout);
+      });
+
+    }, undefined, true);
+};
+// Comment out next line for JS debugging
+window.run();
