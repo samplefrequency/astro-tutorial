838fe30d9d087f00f801aa8c46ee53aef5cb0bb6
Liz Cross <elizabeth@mobify.com>
2015-10-29 09:48:03 -0700

    Fixing indentation on app.js

--- app/app.js
+++ app/app.js
@@ -2,43 +2,43 @@ window.AstroMessages = []; // For debugging messages
 
 window.run = function() {
     require([
-      'astro-full',
-      'bluebird',
-      'application',
-      'plugins/webViewPlugin',
-      'plugins/anchoredLayoutPlugin',
-      'plugins/navigationPlugin'
+        'astro-full',
+        'bluebird',
+        'application',
+        'plugins/webViewPlugin',
+        'plugins/anchoredLayoutPlugin',
+        'plugins/navigationPlugin'
     ],
     function(
-      Astro,
-      Promise,
-      Application,
-      WebViewPlugin,
-      AnchoredLayoutPlugin,
-      NavigationPlugin
+        Astro,
+        Promise,
+        Application,
+        WebViewPlugin,
+        AnchoredLayoutPlugin,
+        NavigationPlugin
     ) {
 
-      // Enter your site url here
-      var baseUrl = 'http://<Your IP Address>:5000';
+        // Enter your site url here
+        var baseUrl = 'http://<Your IP Address>:5000';
 
-      // Initialize plugins
-      var mainNavigationPromise = NavigationPlugin.init();
-      var layoutPromise         = AnchoredLayoutPlugin.init();
+        // Initialize plugins
+        var mainNavigationPromise = NavigationPlugin.init();
+        var layoutPromise = AnchoredLayoutPlugin.init();
 
-      // Start the app by navigating to the base url
-      mainNavigationPromise.then(function(mainNavigationView){
-        mainNavigationView.navigate(baseUrl);
-      });
+        // Start the app by navigating to the base url
+        mainNavigationPromise.then(function(mainNavigationView){
+            mainNavigationView.navigate(baseUrl);
+        });
 
-      // Use the mainNavigationView as the main content view for our layout
-      Promise.join(layoutPromise, mainNavigationPromise, function(layout, mainNavigationView) {
-        layout.setContentView(mainNavigationView);
-      });
+        // Use the mainNavigationView as the main content view for our layout
+        Promise.join(layoutPromise, mainNavigationPromise, function(layout, mainNavigationView) {
+            layout.setContentView(mainNavigationView);
+        });
 
-      // Set our layout as the main view of the app
-      layoutPromise.then(function(layout){
-        Application.setMainViewPlugin(layout);
-      });
+        // Set our layout as the main view of the app
+        layoutPromise.then(function(layout){
+            Application.setMainViewPlugin(layout);
+        });
 
     }, undefined, true);
 };
